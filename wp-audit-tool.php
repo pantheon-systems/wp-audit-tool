1478b64be195ba16be48cce8fc2e543915d2c6c5
Joey Kudish <joey.kudish@pantheon.io>
2024-03-04 12:07:46 -0800

    add Pantheon as author

--- wp-audit-tool.php
+++ wp-audit-tool.php
@@ -3,14 +3,14 @@
  Plugin Name: WP Audit Tool
  Plugin URI: https://github.com/pantheon-systems/wp-audit-tool
  Description: Pantheon's WP Audit Tool is a plugin that provides a suite of tools to help audit a WordPress site's configuration.
- Author: jkudish
+ Author: Pantheon, jkudish
  Version: 1.0.0
  Author URI: https://pantheon.io
  Text Domain: wp-audit-tool
  */
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 require_once __DIR__ . '/lib/health-checks.php';
